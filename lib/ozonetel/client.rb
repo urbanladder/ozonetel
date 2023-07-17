1bd8d4f08181aecfce2a368a5f15cce48afa921c
manishjindal <manish.jindal@urbanladder.com>
2023-07-17 17:59:36 +0530

    CONWEB-8769 , revamp Ozonetel Helper to handle outbound campaign calls and others

--- lib/ozonetel/client.rb
+++ lib/ozonetel/client.rb
@@ -3,67 +3,91 @@ require 'httparty'
 module Ozonetel
   class Client
     include HTTParty
-    MANUAL_DIAL_URL = "http://api1.cloudagent.in/CAServices/AgentManualDial.php"
-    ADD_DATA_URL = "http://api1.cloudagent.in/cloudAgentRestAPI/index.php/CloudAgent/CloudAgentAPI/addCamapaignData"
+    MANUAL_DIAL_URL = "https://in1-ccaas-api.ozonetel.com/CAServices/PhoneManualDial.php"
+    ADD_DATA_URL = "https://in1-ccaas-api.ozonetel.com/cloudAgentRestAPI/index.php/CloudAgent/CloudAgentAPI/addCamapaignData"
+    SCHEDULE_DIAL_URL = "https://in1-ccaas-api.ozonetel.com/CAServices/scheduleCampaignData/scheduleCampaignData.php?"
 
-    def initialize(customer, api_key, campaign_name, did = nil)
-      @user_name = customer
+    def initialize(api_key, user_name)
       @api_key = api_key
-      @campaign_name = campaign_name
-      @did = did
+      @user_name = user_name
     end
 
-    def trigger_cod_confirmation_call(call_data)
-      call_data.merge!({ 'api_key' => @api_key, 'campaign_name' => @campaign_name })
-      call_response = HTTParty.get(ADD_DATA_URL, :query => call_data)
-    end
-
-    def manual_dial_online(type, customer_number, skill = nil, agent_id = nil, uui = nil)
-      if type == "skill_to_phone"
-        raise StandardError.new("Wrong arguments passed") if (skill.nil? or customer_number.nil?)
-        skill_to_phone_dial(customer_number, skill, uui)
-
-      elsif type == "agent_to_phone"
-        raise StandardError.new("Wrong arguments passed") if (agent_id.nil? or customer_number.nil?)
-        skill_to_phone_dial(customer_number, agent_id)
-
-      else
-        raise StandardError.new("Incorrect type value.")
-      end
-    end
-
-    private
-    def skill_to_phone_dial(customer_number, skill,uui)
-      call_data = {
-        'username' => @user_name,
-        'api_key' => @api_key,
-        'did' => @did,
-        'campaignName' => @campaign_name,
-        'skill' => skill,
-        'customerNumber' => customer_number,
-        'uui' => uui
+    def schedule_dial(customer_number, agent_id, time_to_call, uui, skill, priority, campaign_name)
+      bulk_data ={
+        "map" => [
+          "PhoneNumber",                    # required
+          "ScheduledTime",                  # required
+          "AgentID",                        # required for AgentWise Campaigns
+          "Name",
+          "skill",
+          "Priority"
+        ],
+        "data" => [
+          [
+            customer_number,
+            time_to_call,
+            agent_id,
+            uui,
+            skill,
+            priority
+          ]
+        ]
       }
-      call_response = HTTParty.get(MANUAL_DIAL_URL, :query => call_data)
-      parse_response(call_response)
-    end
-
-    private
-    def agent_to_phone_dial(customer_number, agent_id)
       data = {
-        'username' => @user_name,
-        'api_key' => @api_key,
-        'agentID' => agent_id,
-        'customerNumber' => customer_number,
-        'campaignName' => @campaign_name
+        'userName' => @user_name,           # required: (CloudAgent account name)
+        'api_key' => @api_key,              # required: (CloudAgent account name)
+        'campaign_name' => campaign_name,   # required: (Available in CloudAgent Admin login)
+        'bulkData' => bulk_data,            # required: Number from which calls are dialed out
       }
       call_response = HTTParty.post(MANUAL_DIAL_URL, :body => data)
-      parse_response(call_response)
+      parse_response(call_response, 'json')
+    end
+
+    def manual_dial(customer_number, did, agent_name, uui)
+      data = {
+        'userName' => @user_name,           # required: (CloudAgent account name)
+        'apiKey' => @api_key,               # required: (Available in CloudAgent Admin login)
+        'did' => did,                       # required: Number from which calls are dialed out
+        'phoneName' => agent_name,          # required: Name of the offline agent to which Outbound calls are to be assigned
+        'custNumber' => customer_number,    # required: no to be dialed out
+        # 'callLimit' => call_max_limit,    # maximum talktime in seconds
+        'uui' => uui,                       # additional info
+      }
+      call_response = HTTParty.post(MANUAL_DIAL_URL, :body => data)
+      parse_response(call_response, 'json')
+    end
+
+    def add_campaign_call(customer_number, agent_id, uui, campaign_name, skill, priority, expiry, check_duplicate = false)
+      raise ArgumentsError.new("[add_campaign_call] : customer_number is required") if customer_number.nil?
+      data = {
+        'userName' => @user_name,                   # required: (CloudAgent account name)
+        'api_key' => @api_key,                      # required: (Available in CloudAgent Admin login)
+        'campaign_name' => campaign_name,           # required: (Respective campaign name where data to be added)
+        'PhoneNumber' => customer_number,           # required: (data/leads that is to be inserted in Campaign)
+        'agentId' => agent_id,                      # (Parameter to be used for agentwise campaign as per Header in map file)
+        'Name' => uui,                              # (optional name to the PhoneNumber/Lead adding. These details reflect in UUI column of callback, screenpop and CDR report)
+        'checkDuplicate' => check_duplicate,        # (avoids adding duplicate numbers to the Campaign)
+        'action' => "start",                        # (Starts the campaign simultaneous to adding data)
+        'skill' => skill,                           # (Name of the skill through which the calls are to be routed in Skillswise campaigns)
+        'Priority' => priority,                     # (Higher the priority value(1-99) assigned to the phoneNumber, faster the number gets dialed)
+        'ExpiryDate' => expiry.strftime('%Y-%m-%d'),             # (YYYY-MM-DD is the last date the number to be valid in the dialer)
+        'format' => "json",                         # (json/xml)
+      }
+
+      call_response = HTTParty.post(ADD_DATA_URL, :body => data)
+      parse_response(call_response, 'json')
     end
 
     private
-    def parse_response(call_response)
-      message = Hash.from_xml(call_response.parsed_response)['status']
-      {:status => call_response.code, :message => message}
+    def parse_response(call_response, format = 'xml')
+      begin
+        if format == 'json'
+          return {:status => call_response["status"], :code => call_response.code, :message => call_response["message"]}
+        end
+        return {:status => call_response["xml"]["status"], :code => call_response.code, :message => call_response['xml']['message']}
+      rescue Exception => e
+        raise StandardError.new("Server error at Ozonetel Helper: " + e.message)
+      end
     end
   end
 end
